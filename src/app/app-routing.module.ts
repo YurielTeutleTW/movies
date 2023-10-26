34214b3fd3126a20cc3ccf57dbef494bd39f8207
YurielTeutleTW <i19220057.19@puebla.tecnm.mx>
2023-10-26 07:25:49 -0600

    se agrega el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesListComponent } from './components/movies/movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+   path: 'home',
+   component: HomeComponent
+},
+
+{
+  path: 'movies',
+  component: MoviesListComponent
+
+},
+
+{
+  path: '**',
+  pathMatch: 'full',
+  redirectTo: 'home'
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { MoviesListComponent } from './components/movies/movies-list/movies-list.component';
+import { MovieDetailsComponent } from './components/movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MoviesListComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
